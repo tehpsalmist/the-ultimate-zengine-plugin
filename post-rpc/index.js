f75b0d1055b93aedec6401ef0d7efb3e0167641f
Anna Parks <anna@wizehive.com>
2019-08-26 17:23:50 -0600

    please excuse my noob react codez

--- post-rpc/index.js
+++ post-rpc/index.js
@@ -6,9 +6,66 @@ export const client = new Client(document.location.ancestorOrigins[0])
 client.start()
 
 export const getMe = callback => client.call('znHttp', {
-  plugin: { apiVersion: '1.0.1' },
-  params: { method: 'get', url: '/users/me' }
+  options: { apiVersion: '1.0.1' },
+  request: { method: 'get', url: '/users/me' }
 }, callback)
+
+export const getForms = test => client.call('znHttp', {
+  options: { apiVersion: 'v1' },
+  request: { method: 'get', url: '/forms.csv' }
+})
+
+export const getWorkspaces = test => client.call('znHttp', {
+  options: { apiVersion: '1' },
+  request: { method: 'get', url: '/workspaces', params: {id: 1}}
+}, test)
+
+export const uploadFile = (formId, fieldId, file, data) => {
+
+  var reader = new FileReader();
+
+  reader.onload = function(e) {
+    var arrayBuffer = reader.result;
+
+    var promise = client.call('znHttp', {
+      options: { apiVersion: 'v1' },
+      request: {
+        method: 'post',
+        url: `/forms/${formId}/uploads`,
+        data: {
+          fieldId: fieldId,
+          'form[id]': formId
+        },
+        multipart: {
+          key: 'file',
+          file: arrayBuffer,
+          name: file.name,
+          type: file.type
+        }
+      }
+    }, null, 60000)
+
+    promise.then(response => {
+      data = data || {}
+      data[`field${fieldId}`] = response.data.data.hash
+      saveRecord(data, formId).then((response) => {
+        znMessage(`File upload to Record ${response.data.id}`);
+      })
+    })
+  }
+
+  reader.readAsArrayBuffer(file);
+
+}
+
+export const saveRecord = (data, formId) => client.call('znHttp', {
+  options: { apiVersion: '1' },
+  request: {
+    method: 'post',
+    url: `/forms/${formId}/records`,
+    data: data
+  }
+})
 
 export const znConfirm = (message, callback) => {
   console.log('calling confirm')
